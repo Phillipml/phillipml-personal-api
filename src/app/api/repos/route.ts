3f7851622c247822d3dd0814a4260af54fe449e7
Phillipml <contato.phillip.menezes@gmail.com>
2025-10-17 18:56:15 -0300

    headers added

--- src/app/api/repos/route.ts
+++ src/app/api/repos/route.ts
@@ -160,5 +160,24 @@ const repos = [
 ];
 
 export async function GET() {
-  return NextResponse.json(repos);
+  return NextResponse.json(repos, {
+    headers: {
+      "Access-Control-Allow-Origin": "*", // 🌍 permite qualquer origem
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
+
+export async function OPTIONS() {
+  return NextResponse.json(
+    {},
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    }
+  );
 }
